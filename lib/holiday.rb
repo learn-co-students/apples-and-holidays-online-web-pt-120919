cc905ec97976757f5fd21d203ddc93c199a69986
Marcel Rico <personal.mr4@gmail.com>
2019-12-15 18:15:10 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -18,31 +18,36 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #   }
   # }
   # return the second element in the 4th of July array
+  holiday_hash[:summer][:fourth_of_july][1]
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
   # holiday_hash is identical to the one above
   # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
-
+  [:christmas,:new_years].each{|sym| holiday_hash[:winter][sym] << supply}
+  holiday_hash
 end
 
 
 def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
-
+  holiday_hash[:spring][:memorial_day] << supply
+  holiday_hash
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   # code here
   # remember to return the updated hash
-
+  holiday_hash[season][holiday_name] = supply_array
+  holiday_hash
 end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-
+  
+  holiday_hash[:winter].values.flatten
 end
 
 def all_supplies_in_holidays(holiday_hash)
@@ -53,12 +58,31 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
+  
+  
+  holiday_hash.each do |h_key,h_value|
+    puts "#{h_key.capitalize}:"
+    h_value.each do |v_key,v_value|
+      label = v_key.to_s.split("_").collect{|k| k.capitalize}.join(" ")
+      puts "  #{label}: #{v_value.join(", ")}"
+    end
+  end
 
 end
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
+  
+  arr = []
+  holiday_hash.each do |k,v|
+    v.values.length.times do |idx|
+      if v.values[idx].include?("BBQ")
+        arr << v.keys[idx]
+      end
+    end
+  end
+  arr
 
 end
 
